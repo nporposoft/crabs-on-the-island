083af7ce7884d70356ed9dc99ae1e538c3792a4c
Tim Heuett <tim.heuett@gmail.com>
2024-10-04 22:33:18 -0700

    add day night cycle

---
+++ scenes/WorldClock.gd
@@ -0,0 +1,9 @@
+class_name WorldClock
+
+extends Node
+
+var time: float = 0.3
+@export var _time_scale: float = 0.01
+
+func _process(delta: float) -> void:
+	time = fmod(time + (delta * _time_scale), 1.0)
