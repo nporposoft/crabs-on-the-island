1b08a85f1db86d6906a35da3201520c2714f90e8
Kyle P <65381820+kpointerPDX@users.noreply.github.com>
2024-10-04 23:37:24 -0700

    overhauling resources
    = adjusted sprites
    = converted node to RigidBody2D
    + added collisionshapes and area2Ds for harvesting
    + added pdf of short story that inspired this game

--- resource.gd
+++ resource.gd
@@ -1,25 +1,42 @@
-extends Node
+extends RigidBody2D
 
 @onready var sprite = $Sprite2D
+@onready var chunk_collider = $chunk_collider
+@onready var ingot_collider = $ingot_collider
 var chunkTXR := preload("res://assets/chunk.png")
 var ingotTXR := preload("res://assets/ingot.png")
 
 enum MATERIAL_TYPE {COBALT, IRON, SILICON}
 
-@export var material = MATERIAL_TYPE.SILICON
+@export var materialType = MATERIAL_TYPE.SILICON
 @export var amount = 1000.0
 @export var is_chunk = false
 
 
 func _set_resource (_mat, _amount, _isChunk):
-	material = _mat
+	materialType = _mat
 	amount = _amount
 	is_chunk = _isChunk
 	
 	if is_chunk:
 		sprite.set_texture(chunkTXR)
+		chunk_collider.set_disabled(false)
+		ingot_collider.set_disabled(true)
 	else:
 		sprite.set_texture(ingotTXR)
+		ingot_collider.set_disabled(false)
+		chunk_collider.set_disabled(true)
+	
+	match materialType:
+		MATERIAL_TYPE.COBALT:
+			sprite.set_modulate(Color(0.33, 0.33, 1.0))
+			set_mass(amount * 8.8)
+		MATERIAL_TYPE.IRON:
+			sprite.set_modulate(Color(0.4, 0.4, 0.4))
+			set_mass(amount * 7.86)
+		MATERIAL_TYPE.SILICON:
+			sprite.set_modulate(Color(0.6, 1.0, 1.0))
+			set_mass(amount * 2.33)
 
 func _extract (extractAmount) -> float:
 	if extractAmount >= amount:
@@ -32,7 +49,7 @@ func _extract (extractAmount) -> float:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_set_resource(material, amount, is_chunk)
+	_set_resource(materialType, amount, is_chunk)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
