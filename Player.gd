2b92801f9f73912ebc236b21bb979c2c671fe225
Kyle P <65381820+kpointerPDX@users.noreply.github.com>
2024-10-04 21:48:17 -0700

    crab revamp
    = changed crab to RigidBody2D for physics-based movement
    + implemented dodge movement
    + added all(?) attack, pickup, harvest, dodge actions to input map

--- Player.gd
+++ Player.gd
@@ -2,7 +2,19 @@ class_name Player
 
 extends Node
 
-var _character: CharacterBody2D
+var _character: RigidBody2D
+
+var dodgeCooldown = 0.0
+var dodgeCooldownMax = 1.67
+var lateralDodgeMult = 50.0
+var canDodge = true
+
+
+func _tickCooldowns(_delta) -> void:
+	if dodgeCooldown > 0.0:
+		dodgeCooldown -= _delta
+		if dodgeCooldown <= 0.0:
+			canDodge = true
 
 func _ready() -> void:
 	_character = $Crab
@@ -11,14 +23,51 @@ func _process(delta: float) -> void:
 	var moveDir = Vector2(0.0, 0.0)
 	if Input.is_action_pressed("move_up"):
 		moveDir.y = -1.0
+		if Input.is_action_pressed("dodge") and canDodge:
+			if Input.is_action_pressed("move_left"):
+				_character.apply_central_impulse(Vector2(-1.0, -1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			elif Input.is_action_pressed("move_right"):
+				_character.apply_central_impulse(Vector2(1.0, -1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			else:
+				_character.apply_central_impulse(Vector2(0.0, -1.0) * _character._stats.move_speed * lateralDodgeMult)
+			dodgeCooldown = dodgeCooldownMax
+			canDodge = false
 	if Input.is_action_pressed("move_down"):
 		moveDir.y = 1.0
+		if Input.is_action_pressed("dodge") and canDodge:
+			if Input.is_action_pressed("move_left"):
+				_character.apply_central_impulse(Vector2(-1.0, 1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			elif Input.is_action_pressed("move_right"):
+				_character.apply_central_impulse(Vector2(1.0, 1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			else:
+				_character.apply_central_impulse(Vector2(0.0, 1.0) * _character._stats.move_speed * lateralDodgeMult)
+			dodgeCooldown = dodgeCooldownMax
+			canDodge = false
 	if Input.is_action_pressed("move_left"):
 		moveDir.x = -1.0
+		if Input.is_action_pressed("dodge") and canDodge:
+			if Input.is_action_pressed("move_up"):
+				_character.apply_central_impulse(Vector2(-1.0, -1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			elif Input.is_action_pressed("move_down"):
+				_character.apply_central_impulse(Vector2(-1.0, 1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			else:
+				_character.apply_central_impulse(Vector2(-1.0, 0.0) * _character._stats.move_speed * lateralDodgeMult)
+			dodgeCooldown = dodgeCooldownMax
+			canDodge = false
 	if Input.is_action_pressed("move_right"):
 		moveDir.x = 1.0
+		if Input.is_action_pressed("dodge") and canDodge:
+			if Input.is_action_pressed("move_up"):
+				_character.apply_central_impulse(Vector2(1.0, -1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			elif Input.is_action_pressed("move_down"):
+				_character.apply_central_impulse(Vector2(1.0, 1.0).normalized() * _character._stats.move_speed * lateralDodgeMult)
+			else:
+				_character.apply_central_impulse(Vector2(1.0, 0.0) * _character._stats.move_speed * lateralDodgeMult)
+			dodgeCooldown = dodgeCooldownMax
+			canDodge = false
 	
-	var moveActual = moveDir.normalized()
+	_character.apply_central_impulse(moveDir.normalized() * _character._stats.move_speed)
+	
 	
 	#var input: Vector2
 	#input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -26,3 +75,5 @@ func _process(delta: float) -> void:
 	#_character.move(input.normalized())
 
 	$Camera2D.position = _character.position
+	
+	_tickCooldowns(delta)
