72fe19d9ea163c8b01168cbae0301570515512a1
Kyle P <65381820+kpointerPDX@users.noreply.github.com>
2024-10-04 19:56:41 -0700

    minor change to player

--- Player.gd
+++ Player.gd
@@ -8,9 +8,21 @@ func _ready() -> void:
 	_character = $Crab
 
 func _process(delta: float) -> void:
-	var input: Vector2
-	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	input.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	_character.move(input.normalized())
+	var moveDir = Vector2(0.0, 0.0)
+	if Input.is_action_pressed("move_up"):
+		moveDir.y = -1.0
+	if Input.is_action_pressed("move_down"):
+		moveDir.y = 1.0
+	if Input.is_action_pressed("move_left"):
+		moveDir.x = -1.0
+	if Input.is_action_pressed("move_right"):
+		moveDir.x = 1.0
+	
+	var moveActual = moveDir.normalized()
+	
+	#var input: Vector2
+	#input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	#input.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	#_character.move(input.normalized())
 
 	$Camera2D.position = _character.position
