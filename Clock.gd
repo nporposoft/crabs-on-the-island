1e5e6636a2bd75cbc56c1461a74a3ce31163b2f6
Tim Heuett <tim.heuett@gmail.com>
2024-10-05 03:28:55 -0700

    add days to clock (for UI??)

--- Clock.gd
+++ Clock.gd
@@ -3,8 +3,12 @@ class_name Clock
 extends Node
 
 var time: float = 0.3
+var day_count: int
 @export var _time_scale: float = 0.01
 
 
 func _process(delta: float) -> void:
-	time = fmod(time + (delta * _time_scale), 1.0)
+	time += delta * _time_scale
+	if time > 1.0:
+		day_count += 1
+		time = fmod(time, 1.0)
