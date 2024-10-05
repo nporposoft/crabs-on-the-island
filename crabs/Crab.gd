0c18359afa02ab90ae98b772ece1476b6a2159ed
Tim Heuett <tim.heuett@gmail.com>
2024-10-04 23:41:45 -0700

    drain/deplete battery energy

--- crabs/Crab.gd
+++ crabs/Crab.gd
@@ -2,14 +2,24 @@ class_name Crab
 
 extends RigidBody2D
 
+@export var move_battery_usage: float = 0.1
+@export var dash_battery_usage: float = 0.5
+@export var harvest_battery_usage: float = 0.1
+
 var _body_resources: Dictionary
-var _carried_resources: Dictionary
+var _carried_resources: Dictionary = {
+	"iron": 0,
+	"cobalt": 0,
+	"silicon": 0,
+	"water": 0,
+	"battery_energy": 0.0,
+}
 var _stats: Dictionary = {
 	"size": 1,
 	"hit_points": 10,
 	"strength": 10,
 	"move_speed": 10.0,
-	"solar_efficiency": 10,
+	"solar_efficiency": 0.1,
 	"battery_capacity": 10,
 	"harvest_speed": 10
 }
@@ -22,9 +32,34 @@ func init(body_resources: Dictionary, stats: Dictionary) -> void:
 	_stats = stats
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	_update_animation_state()
+	_harvest_sunlight(delta)
+	_deplete_battery_from_movement(delta)
+
+
+func _harvest_sunlight(delta: float) -> void:
+	var time: float = WorldClock.time
+	if time > 0.25 && time < 0.75:
+		var gained_energy: float = _stats.solar_efficiency * delta
+		_set_battery_energy(_carried_resources.battery_energy + gained_energy)
+
+
+func _deplete_battery_from_movement(delta: float) -> void:
+	if !_is_moving():
+		return
+	
+	var lost_energy: float = move_battery_usage * delta
+	_set_battery_energy(_carried_resources.battery_energy - lost_energy)
+
+
+func _set_battery_energy(value: float) -> void:
+	_carried_resources.battery_energy = clampf(value, 0, _stats.battery_capacity)
+
+
+func _update_animation_state() -> void:
 	var animation: String
-	if (linear_velocity.length() > 1):
+	if _is_moving():
 		animation = "move"
 	else:
 		animation = "idle"
@@ -32,3 +67,7 @@ func _process(_delta: float) -> void:
 	if animation != _current_animation:
 		_current_animation = animation
 		$AnimatedSprite2D.play(_current_animation)
+
+
+func _is_moving() -> bool:
+	return linear_velocity.length() > 1
