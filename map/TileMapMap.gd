0c1b8f3f1a568287b97c4b3750899efbc40e77a9
Tim Heuett <tim.heuett@gmail.com>
2024-10-29 05:37:31 -0700

    add error handling for out of bounds tilemap calls

--- map/TileMapMap.gd
+++ map/TileMapMap.gd
@@ -3,19 +3,25 @@ class_name TileMapMap
 
 extends Map
 
+@onready var tilemap: TileMapLayer = $tileIsland2
 
 func get_terrain_at_point(point: Vector2) -> TerrainData:
-	var cell: Vector2             = $tileIsland.local_to_map(point)
-	var cell_data                 = $tileIsland.get_cell_tile_data(cell)
 	var terrain_data: TerrainData = TerrainData.new()
+	
+	var cell: Vector2 = tilemap.local_to_map(to_local(point))
+	var cell_data: TileData = tilemap.get_cell_tile_data(cell)
+	if cell_data == null:
+		push_warning("attempt to get cell data at ", cell, " for point ", point, " is out of map bounds")
+		return terrain_data
+	
 	terrain_data.harvest_type = TerrainData.harvest_type_from_string(cell_data.get_custom_data(harvestTypeKey))
 	#terrain_data.sound_type = cell_data.get_custom_data(soundTypeKey)
 	return terrain_data
 
 
 func get_terrain_in_radius(point: Vector2, radius: float) -> Array[TerrainData]:
-	var center_on_map: Vector2i = $tileIsland.world_to_map(point)
-	var tileset: TileSet = $tileIsland.tile_set
-	var tile_size: int = $tileIsland.cell_size
+	var center_on_map: Vector2i = tilemap.world_to_map(point)
+	var tileset: TileSet = tilemap.tile_set
+	var tile_size: int = tilemap.cell_size
 	
 	return []
