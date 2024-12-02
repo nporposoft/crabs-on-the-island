5cf7ce6178f683cd050b28c542cfd672534363f9
Tim Heuett <tim.heuett@gmail.com>
2024-12-01 16:07:38 -0800

    fix warning

--- game/UI/fps_counter.gd
+++ game/UI/fps_counter.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 	DebugMode.on_change.connect(set_visible)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	set_text(str(Engine.get_frames_per_second()) + " fps")
