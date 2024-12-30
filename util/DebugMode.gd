cff3257758cb469d2d3c47955d24983a8a4548bb
Tim Heuett <tim.heuett@gmail.com>
2024-12-29 16:05:40 -0800

    log debug mode toggle

--- util/DebugMode.gd
+++ util/DebugMode.gd
@@ -6,4 +6,5 @@ var enabled: bool
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug"):
 		enabled = !enabled
+		print("Debug mode set to ", enabled)
 		on_change.emit(enabled)
