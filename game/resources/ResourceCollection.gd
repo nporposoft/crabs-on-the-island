32adb49cee32f0ef04b6f54c3d4f99c4829e3054
Tim Heuett <tim.heuett@gmail.com>
2024-12-29 16:26:58 -0800

    remove problematic type assertions

--- game/resources/ResourceCollection.gd
+++ game/resources/ResourceCollection.gd
@@ -27,19 +27,13 @@ func nearest_resource(position: Vector2) -> Node2D:
 
 
 func crabs() -> Array:
-	return all().filter(func(resource: Node2D) -> bool:
+	return all().filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Crab
 	)
 
 
-# E 0:01:39:0923   ResourceCollection.gd:36 @ crabs_by_distance(): Error calling method from 'filter': 'GDScript::<anonymous lambda>': Cannot convert argument 1 from Object to Object
-#  <C++ Error>    Method/function failed. Returning: Array()
-#  <C++ Source>   core/variant/array.cpp:514 @ filter()
-#  <Stack Trace>  ResourceCollection.gd:36 @ crabs_by_distance()
-#                 CrabAI.gd:131 @ _harvest_crab_routine()
-#                 CrabAI.gd:51 @ _process()
 func crabs_by_distance(position: Vector2) -> Array:
-	return by_distance(position).filter(func(resource: Node2D) -> bool:
+	return by_distance(position).filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Crab
 	)
 
@@ -51,13 +45,13 @@ func nearest_crab(position: Vector2) -> Crab:
 
 
 func morsels() -> Array:
-	return all().filter(func(resource: Node2D) -> bool:
+	return all().filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Morsel
 	)
 
 
 func morsels_by_distance(position: Vector2) -> Array:
-	return by_distance(position).filter(func(resource: Node2D) -> bool:
+	return by_distance(position).filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Morsel
 	)
 
@@ -69,13 +63,13 @@ func nearest_morsel(position: Vector2) -> Morsel:
 
 
 func water() -> Array:
-	return all().filter(func(resource: Node2D) -> bool:
+	return all().filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Water
 	)
 
 
 func water_by_distance(position: Vector2) -> Array:
-	return by_distance(position).filter(func(resource: Node2D) -> bool:
+	return by_distance(position).filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Water
 	)
 
@@ -87,13 +81,13 @@ func nearest_water(position: Vector2) -> Water:
 
 
 func sand() -> Array:
-	return all().filter(func(resource: Node2D) -> bool:
+	return all().filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Sand
 	)
 
 
 func sand_by_distance(position: Vector2) -> Array:
-	return by_distance(position).filter(func(resource: Node2D) -> bool:
+	return by_distance(position).filter(func(resource) -> bool:
 		return is_instance_valid(resource) and resource is Sand
 	)
 
