8170e6ae204f600fc2fe799ff768023e05e7a8e3
Tim Heuett <tim.heuett@gmail.com>
2024-12-31 13:58:58 -0800

    fix health bar

--- game/crabs/health_bar.gd
+++ game/crabs/health_bar.gd
@@ -5,12 +5,17 @@ extends ProgressBar
 @onready var label: Label = $healthNum
 @onready var crab: Crab = get_parent()
 
+var _visibility_triggered: bool
+
 func _ready() -> void:
-	crab.on_damage.connect(func() -> void: set_visible(true))
+	DebugMode.on_change.connect(set_visible)
+	crab.on_damage.connect(func() -> void: _visibility_triggered = true)
 
 
 func _process(_delta: float) -> void:
-	if !is_visible || !DebugMode.enabled: return
+	set_visible(DebugMode.enabled or _visibility_triggered)
+	if !is_visible():
+		return
 
 	var hp: float = crab._HP
 	var hp_percent: float = (hp / crab._stats_effective.hit_points) * 100.0
